36e8c8e26a7d2c63f9a1d6aa7b4df51d10503727
Levi Fernandes <64471627+levifernands@users.noreply.github.com>
2021-07-05 15:53:49 -0300

    main.js updated
    new feature added to the website

--- scripts/main.js
+++ scripts/main.js
@@ -1,63 +1,118 @@
-const popup = document.querySelector(".popup-wrapper")
-function abrir(){
-    popup.style.display = "flex"
-}
+const popup = document.querySelector(".popup");
 
-function fechar(){
-    popup.style.display = "none"
-}
+const conteudoColuna = document.querySelector('.conteudoColuna');
+
+const inputDescricao = document.querySelector('#inputDescription');
+const selectTipo = document.querySelector('#selectType');
+const selectResponsavel = document.querySelector('#selectResponsible');
+
+const coluna = document.querySelector('.coluna');
 
 
-const formularioDescricao = document.querySelector('#description')
-const formularioTipo = document.querySelector('#Type')
-const formularioResponsavel = document.querySelector('#Name')
-const formularioBotao = document.querySelector('#botaoTicket')
-const colunaTicket = document.querySelector('.conteudoDentroDaColuna')
+var tarefas = [];
 
-const Tickets = []
+function abrirPopup() {
+    popup.style.display = "flex";
+};  
 
-function mostrarTicket(){
+function fecharPopup() {
+    inputDescricao.value = '';
+    selectTipo.selectedIndex = '';
+    selectResponsavel.selectedIndex = '';
+    popup.style.display = "none";
+};
 
-    colunaTicket.innerHTML = ''
-    for (Ticket of Tickets){
-        const elementoTicket = document.createElement('div')
-        elementoTicket.classList.add('Ticket')
 
-        const elementoTipoDoTicket = document.createElement('div')
-        elementoTipoDoTicket.classList.add('tipodoticket')
+function mostrarTickets() {
+    conteudoColuna.innerHTML = '';
+
+    for (tarefa of tarefas) {
+
+        const descricao = document.createTextNode(tarefa.descricao);
+        const tipo = document.createTextNode(tarefa.tipo);
+        const responsavel = document.createTextNode(tarefa.responsavel);
+        const id = document.createTextNode(tarefa.id);
+
+
+        const novoTicket = document.createElement('div');
+        novoTicket.classList.add('ticket');
+
+        const novoProcedimento = document.createElement('div');
+        novoProcedimento.classList.add('tipoTicket');
         
-        const elementoDescricao = document.createElement('div')
-        elementoDescricao.classList.add('descrição')
+        const novaDescricao = document.createElement('div');
+        novaDescricao.classList.add('descricao');
+
+        const negrito = document.createElement('b');
+
+        const novoCodigo = document.createElement('div');
+        novoCodigo.classList.add('codigo');
+
+        const novoResponsavel = document.createElement('div');
+        novoResponsavel.classList.add('nomeResponsavel');
+
+        const novoRodape = document.createElement('div');
+        novoRodape.classList.add('rodape');
+
+        const novoBotao = document.createElement('button');
+        novoBotao.setAttribute('onclick', 'removerTicket('+ tarefa.id +')');
+
+        const imagemBotao = document.createElement('img');
+        imagemBotao.src = "imagens/ret.png";
+
+
+        novaDescricao.appendChild(descricao);
+        novoProcedimento.appendChild(tipo);
+        novoResponsavel.appendChild(responsavel);
+        negrito.appendChild(id);
+        novoCodigo.appendChild(negrito);
         
-        const elementoResponsavel = document.createElement('div')
-        elementoResponsavel.classList.add('nome')
+        novoTicket.appendChild(novoResponsavel);    
+        novoTicket.appendChild(novoProcedimento);
+        novoTicket.appendChild(novoCodigo);
+        novoTicket.appendChild(novaDescricao);
+        novoTicket.appendChild(novoRodape);
+
+        novoRodape.appendChild(novoResponsavel);
+        novoRodape.appendChild(novoBotao);
+        novoRodape.appendChild(imagemBotao);
         
-        
-        const TextoTicket = document.createTextNode(formularioDescricao.value)
-        const tipoTicket = document.createTextNode(formularioTipo.options[formularioTipo.selectedIndex].text)
-        const ResponsavelTicket = document.createTextNode(formularioResponsavel.options[formularioResponsavel.selectedIndex].text)
+        novoBotao.appendChild(imagemBotao);
 
+        conteudoColuna.appendChild(novoTicket);
+    };
+};
 
-        colunaTicket.appendChild(elementoTicket)
-
-             elementoTicket.appendChild(elementoDescricao) 
-             elementoTicket.appendChild(elementoTipoDoTicket)
-             elementoTicket.appendChild(elementoResponsavel)
-
-                  elementoDescricao.appendChild(TextoTicket)
-                  elementoTipoDoTicket.appendChild(tipoTicket)
-                  elementoResponsavel.appendChild(ResponsavelTicket)
-    }
-}
-
-mostrarTicket()
+mostrarTickets();
 
 function addTicket(){
-    const textoTicket = formularioDescricao.value
-    Tickets.push(textoTicket)
-    formularioDescricao.value = ''
-    mostrarTicket()
-}
+    const valorInputDescricao = inputDescricao.value;
+    const valorSelectTipo = selectTipo.value;
+    const valorSelectResponsavel = selectResponsavel.value;
+
+    const novoTicket = {
+        id: Math.floor(1000 + Math.random() * 9000),
+        descricao: valorInputDescricao,
+        tipo: valorSelectTipo,
+        responsavel: valorSelectResponsavel
+    };
 
 
+    if (valorInputDescricao == '' || valorSelectTipo == '' || valorSelectResponsavel == '') { 
+        alert("Preencha o formulário");
+    }else {
+        tarefas.push(novoTicket);
+    
+        mostrarTickets();
+        fecharPopup();
+    };
+     
+};
 
+
+function removerTicket(id) {
+    tarefas = tarefas.filter(function(tarefa){
+        return tarefa.id != id;
+    });
+    mostrarTickets();
+};
